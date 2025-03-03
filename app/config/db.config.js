103ca41c0ac9011eb39c95861371bcc82fa6abb2
phamdat <dat@undercurrent.tech>
2025-03-03 21:58:33 +0700

    set db as env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
-    url: "mongodb+srv://wefit365:wefit365@cluster0.h63jd.mongodb.net/wefit365?retryWrites=true&w=majority&appName=Cluster0"
+    url: process.env.DATABASE_URL
 };
